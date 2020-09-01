e75bb8e608eef8f068324a6908ee9749660a4763
antonalpsten <anton.alpsten@elev.ga.ntig.se>
2020-09-01 12:40:54 +0200

    upp 2

--- helloworld/nukorvi/Program.cs
+++ helloworld/nukorvi/Program.cs
@@ -5,8 +5,38 @@ namespace nukorvi
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+         
+            Console.WriteLine("hello bomers!!!");
+
+            Console.WriteLine("skriv ditt namn!");
+
+            string User = Console.ReadLine();
+
+            Console.WriteLine("skriv din ålder!");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("är du vid liv?");
+
+            string alive = Console.ReadLine();
+
+            if(alive == "ja")
+            {
+                Console.WriteLine(User);
+                Console.WriteLine(age);
+                Console.WriteLine("yay maoder f**ker!!");
+            }
+            else
+            {
+                Console.WriteLine(User);
+                Console.WriteLine(age);
+                Console.WriteLine(":(");
+            }
+
+
+
+
         }
     }
 }
