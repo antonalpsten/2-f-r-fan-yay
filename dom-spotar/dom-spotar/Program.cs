1817cc338efa1eed0be1bcf560215af8fe292969
antonalpsten <anton.alpsten@elev.ga.ntig.se>
2020-10-11 23:11:01 +0200

    for now

---
+++ dom-spotar/dom-spotar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dom_spotar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int steg = 1;
+            string message = "hej";
+
+            char[] alfabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z', '_', '.'};
+           string meddelande[message.Length];
+        }
+    }
+}
